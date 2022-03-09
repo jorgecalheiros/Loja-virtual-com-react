091c33f04210a158dab0979b4dc56f86547374b2
jorge <jorgecalheiros.s@gmail.com>
2022-03-08 23:30:15 -0300

    criando as primerias paginas para os formularios

---
+++ src/Pages/Admin/CreateUpdateProdutos.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react"
+
+function CreateUpdateProdutos({ title }) {
+    return (
+        <div>
+            <h1>Formulario para criar produtos</h1>
+        </div>
+    )
+}
+
+export default CreateUpdateProdutos;
--- src/Pages/Admin/Home.js
+++ src/Pages/Admin/Home.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 function Home() {
     return (
         <div>
