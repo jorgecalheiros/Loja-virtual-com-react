80b8331a19f524d43b91fe49617f34172f651153
jorge <jorgecalheiros.s@gmail.com>
2022-03-10 22:38:56 -0300

    services

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,29 @@
-//axios
+import axios from "axios";
 
 class Api {
+    httpClient;
 
+    constructor() {
+        this.httpClient = axios.create({
+            baseURL: "http://localhost",
+        });
+    }
+
+    get(endpoint) {
+        return this.httpClient.get(endpoint);
+    }
+
+    post(endpoint, data) {
+        return this.httpClient.post(endpoint, data);
+    }
+
+    put(endpoint, data) {
+        return this.httpClient.put(endpoint, data);
+    }
+
+    delete(endpoint) {
+        return this.httpClient.delete(endpoint);
+    }
 }
 
 export default new Api;
---
+++ src/Services/ProdutoService.js
@@ -0,0 +1,42 @@
+import Api from "./Api";
+
+
+class ProdutoService {
+    async index() {
+        try {
+            const response = await Api.get("/api/produtos");
+            return [response.data, false];
+        } catch (error) {
+            return [null, error];
+        }
+    }
+
+    async create(data) {
+        try {
+            const response = await Api.post("/api/produtos", data);
+            return [response.data, false];
+        } catch (error) {
+            return [null, error];
+        }
+    }
+
+    async update(id, data) {
+        try {
+            const response = await Api.put(`api/produtos/${id}`, data);
+            return [response.data, false];
+        } catch (error) {
+            return [null, error];
+        }
+    }
+
+    async destroy(id) {
+        try {
+            const response = await Api.delete(`/api/produtos/${id}`);
+            return [response.data, false];
+        } catch (error) {
+            return [null, error];
+        }
+    }
+}
+
+export default new ProdutoService;
